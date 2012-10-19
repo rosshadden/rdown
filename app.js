5622df8be912617e39d81c2b400c0017227a8b6f
Ross Hadden <rosshadden@gmail.com>
2012-10-19 09:54:06 -0400

    Start adding styles.

--- app.js
+++ app.js
@@ -4,16 +4,20 @@ var	program = require('commander'),
 	parse = require('ghm').parse,
 	fs = require('fs');
 
+var styles = '<style></style>\n\n';
+// var styles = '<style>' + fs.readFileSync('./github.css', 'utf8') + '</style>\n\n';
+
+
 program
 	.version('0.0.1')
 	.command('parse <file>')
 	.description('Parse a given input <file> into an output .html file.')
 	.option('-o, --output [file]', 'Save generated output to given [file].', 'output.html')
 	.action(function(file, options){
-		console.log('parsing:', file, options.output);
+		console.log('parsing:', file, '->', options.output);
 
 		var contents = fs.readFileSync(file, 'utf8');
-		var output = parse(contents);
+		var output = styles + parse(contents);
 
 		fs.writeFileSync(options.output, output, 'utf8');
 	});
