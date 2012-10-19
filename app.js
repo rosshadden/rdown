37c5389586067754eaefc9413a8d464c7453c856
Ross Hadden <rosshadden@gmail.com>
2012-10-19 08:41:19 -0400

    Added commander.js.

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 #!/usr/bin/env node
 
-var md = require('ghm').parse;
+var	program = require('commander'),
+	md = require('ghm').parse;
 
-console.log(md('hi'));
+program
+	.version('0.0.1')
+	.command('parse <file>')
+	.description('Parse a given input file <file> into an output .html file.')
+	.option('-o, --output <file>', 'Save generated output to given file [file].', 'file.html')
+	.action(function(file){
+		console.log('parse', arguments);
+	});
+
+program.parse(process.argv);
