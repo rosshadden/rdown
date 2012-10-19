a5dd8e01ec286ab1aad5d4c8b640f7346416fe57
Ross Hadden <rosshadden@gmail.com>
2012-10-19 08:56:33 -0400

    Actual parsing and file generation working.

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 #!/usr/bin/env node
 
 var	program = require('commander'),
-	md = require('ghm').parse;
+	parse = require('ghm').parse,
+	fs = require('fs');
 
 program
 	.version('0.0.1')
 	.command('parse <file>')
-	.description('Parse a given input file <file> into an output .html file.')
-	.option('-o, --output <file>', 'Save generated output to given file [file].', 'file.html')
-	.action(function(file){
-		console.log('parse', arguments);
+	.description('Parse a given input <file> into an output .html file.')
+	.option('-o, --output [file]', 'Save generated output to given [file].', 'output.html')
+	.action(function(file, options){
+		console.log('parsing:', file, options.output);
+
+		var contents = fs.readFileSync(file, 'utf8');
+		var output = parse(contents);
+
+		fs.writeFileSync(options.output, output, 'utf8');
 	});
 
 program.parse(process.argv);
